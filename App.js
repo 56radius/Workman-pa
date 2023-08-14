be86cb0f32c85d91965aee12ee8b93e51de85f49
56radius <cussmeritmohammed@gmail.com>
2023-08-14 05:50:29 +0100

    Second Commit

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import * as React from "react";
+
+//stack navigation imports
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+//screens and compoents imports
+import HomeScreen from "./src/screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> Welcome </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* Home Screen navigation */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+
+        {/* Login Screen navigation */}
+
+        {/* Sign up screen navigation */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,38 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text> Welcome to home screen </Text>
+      {/* Header */}
+      <View style={styles.header}>
+        <Image
+          style={{
+            width: 30,
+            height: 30,
+            borderRadius: 50,
+          }}
+          source={require("../.././assets/water.png")}
+        />
+        <Text style={{ fontWeight: "bold" }}> Workman-pa </Text>
+      </View>
+
+      {/* Welcome Words and paragraph */}
+      <View>
+        <Text>
+          {" "}
+          Welcome To Us {"\n"} <Text> Wassup </Text>{" "}
+        </Text>
+      </View>
+
+      {/* path to login and sign up pages */}
+      {/* 
+      <View style={styles.path}>
+        <TouchableOpacity>
+          <Text> Hello </Text>
+        </TouchableOpacity>
+      </View>*/}
     </View>
   );
 }
@@ -13,6 +40,12 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  header: {
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
   },
